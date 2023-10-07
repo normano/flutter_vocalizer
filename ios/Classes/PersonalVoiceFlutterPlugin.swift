21f0a892c0fb337d5ce742793e61a4dffbe2dd69
Steven Rockarts <stevenrockarts@gmail.com>
2023-10-07 12:39:58 -0600

    Request permission

--- ios/Classes/PersonalVoiceFlutterPlugin.swift
+++ ios/Classes/PersonalVoiceFlutterPlugin.swift
@@ -1,19 +1,50 @@
 import Flutter
 import UIKit
+import AVFoundation
 
 public class PersonalVoiceFlutterPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "personal_voice_flutter", binaryMessenger: registrar.messenger())
-    let instance = PersonalVoiceFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "personal_voice_flutter", binaryMessenger: registrar.messenger())
+        let instance = PersonalVoiceFlutterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "requestPersonalVoiceAuthorization":
+            requestPersonalVoiceAuthorization(result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    private func requestPersonalVoiceAuthorization(result: @escaping FlutterResult) {
+        if #available(iOS 17.0, *) {
+            AVSpeechSynthesizer.requestPersonalVoiceAuthorization() { status in
+                switch status {
+                case .notDetermined:
+                    result("notDetermined")
+                case .denied:
+                    result("denied")
+                    /**
+                     Personal voices are unsupported on this device.
+                     */
+                case .unsupported:
+                    result("unsupported")
+                    /**
+                     The user granted your app's request to use personal voices.
+                     */
+                case .authorized:
+                    result("authorized")
+                default:
+                    print("Default")
+                }
+            }
+        } else {
+            result("Must use iOS 17 or higher to use personal voice")
+        }
+    }
+    
 }
